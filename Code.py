da27ff454afddfad720e831b5ac4f21541038a79
Tad Hatfield <143562958+Thatfield003@users.noreply.github.com>
2024-04-03 12:52:58 -0500

    Outline

--- Code.py
+++ Code.py
@@ -1,2 +1,16 @@
-print("Hello World")
-print("Testing")
+"""
+Mathematical Model of White Mold on Soybean Yield
+Created by Tanner Byer and Tad Hatfield
+Spring 2024
+"""
+
+# Parameters
+
+
+
+# Model
+
+
+
+# Plotting
+
